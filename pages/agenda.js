f4f7514f55e1cc92d2c085c81db18864d9d4bc42
Herllon Cardoso <cardoso.herllon@gmail.com>
2021-04-05 22:32:59 -0300

    initial commit

---
+++ pages/agenda.js
@@ -0,0 +1,7 @@
+export default function Agenda() {
+  return (
+    <div>
+      Agenda
+    </div>
+  )
+}
---
+++ pages/schedule.js
@@ -0,0 +1,7 @@
+export default function Schedule() {
+  return (
+    <div>
+      Schedule
+    </div>
+  )
+}
